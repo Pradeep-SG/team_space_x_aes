b6b646aa6789ac24423f9423c5d59b6ffc831a9b
pradeep.ganesan <pradeep.ganesan@zohocorp.com>
2023-05-09 21:02:56 +0530

    Add first login feature for welcome page

--- welcome.js
+++ welcome.js
@@ -1,39 +1,24 @@
-// let firstLogin = localStorage.getItem("first_login") === "true";
-// if (firstLogin) {
-//     document.addEventListener("DOMContentLoaded", function() {
-//         setTimeout(function() {
-//             var welcomeElement = $("#welcome");
-//             welcomeElement.addClass("fade-out");
-//         }, 3000); // 3 seconds
-    
-//         setTimeout(function() {
-//             var welcomeElement = $("#welcome-2");
-//             welcomeElement.addClass("fade-out");
-//         }, 6000); // 6 seconds
-//         localStorage.setItem("first_login", "false");
-//     });
-// } else {
-//     let welcomeElement = $("#welcome");
-//     if (welcomeElement) {
-//         welcomeElement.css("display", "none");
-//     }
-//     welcomeElement = $("#welcome2");
-//     if (welcomeElement) {
-//         welcomeElement.css("display", "none");
-//     }
-// }
+let firstLogin = localStorage.getItem("first_login") === "true";
+if (firstLogin) {
+    document.addEventListener("DOMContentLoaded", function() {
+        setTimeout(function() {
+            let welcomeElement = $("#welcome");
+            welcomeElement.addClass("fade-out");
+        }, 3000); // 3 seconds
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     setTimeout(function () {
-//         setTimeout(function() {
-//         let welcomeElement = $("#welcome");
-//         welcomeElement.addClass("fade-out");
-//     }, 3000); // 3 seconds
-
-
-//     setTimeout(function() {
-//         let welcomeElement = $("#welcome-2");
-//         welcomeElement.addClass("fade-out");
-//     }, 6000); // 6 seconds
-//     }, 100);
-// });
+        setTimeout(function() {
+            let welcomeElement = $("#welcome-2");
+            welcomeElement.addClass("fade-out");
+        }, 6000); // 6 seconds
+        localStorage.setItem("first_login", "false");
+    });
+} else {
+    let welcomeElement = $("#welcome");
+    if (welcomeElement) {
+        welcomeElement.css("display", "none");
+    }
+    welcomeElement = $("#welcome-2");
+    if (welcomeElement) {
+        welcomeElement.css("display", "none");
+    }
+}
