cd82c42a21f365300762771dc3f9aeac713e1c59
pradeep.ganesan <pradeep.ganesan@zohocorp.com>
2023-05-10 21:28:57 +0530

    Change name of temperature system

--- channels.js
+++ channels.js
@@ -8,14 +8,14 @@ class Channel {
 
 export const channels = {
     "tyrepressure": new Channel(2117208, 643506, 643507),
-    "temperature": new Channel(2117963, 643529, 643510),
+    "fireindication": new Channel(2117963, 643529, 643510),
     "airbag": new Channel(2117973, 643523, 643524),
     "oilsystem": new Channel(2117975, 643526, 643527)
 }
 
 export const channelNames = {
     "tyrepressure": "Tyre pressure system",
-    "temperature": "Temperature System",
+    "fireindication": "Fire indication system",
     "airbag": "Airbag system",
     "oilsystem": "Oil level system"
 }
